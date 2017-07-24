412465ddae69d4092256dee32136a9ea70530260
Evan Harris <echarris@smcm.edu>
2017-07-24 17:00:41 -0400

    Adds styling to nav-bar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './navigation-bar.css';
 
 export default function NavigationBar(props) {
     return (
         <div className="navigation-bar">
             <nav className="navigation-bar-nav">
-                <ul>
+                <ul className="clearfix">
                     <li> 
                         <a href="https://www.google.com/" target="_BLANK" rel="noopener noreferrer">What ?</a>
                     </li>
